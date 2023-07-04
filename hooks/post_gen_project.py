88278a5acd721c282abb971755eb9f85744a7d86
HinnyTsang <mhstsangab@connect.ust.hk>
2023-07-05 00:59:39 +0800

    Add hooks for cookie cutter

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,8 @@
+"""Post generate hook"""
+
+print(
+    """
+    Finish generating project structure with cookie cutter.
+    Thanks for using Std Tech Dept Template.
+    """
+)
---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,12 @@
+"""Pre generate hook"""
+
+print(
+    """
+    =======================================================
+    |                                                     |
+    |               Std Tech Dept Template                |
+    |                                                     |
+    =======================================================
+    Generating default project structure with cookie cutter.
+    """
+)
