230fe8ddc79153088024c018825f707afdc52438
HinnyTsang <mhstsangab@connect.ust.hk>
2023-07-09 19:03:15 +0800

    Validate author email in pre-commit hook

--- py-basic/hooks/pre_gen_project.py
+++ py-basic/hooks/pre_gen_project.py
@@ -1,3 +1,9 @@
 """Pre generate hook"""
+import re
 
-print("Generating project")
+def validate_author_email(email: str):
+    """Validate email"""
+    email_pattern = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
+    assert re.match(email_pattern, email), "Invalid email"
+
+validate_author_email("{{ cookiecutter.author_email }}")
