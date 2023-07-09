43232a256dfe894730d76b4c937ac4fb72b27e3d
HinnyTsang <mhstsangab@connect.ust.hk>
2023-07-09 18:59:27 +0800

    Move hooks to correct location

---
+++ py-basic/hooks/post_gen_project.py
@@ -0,0 +1,9 @@
+"""Post generate hook"""
+from textwrap import dedent
+
+print(dedent(
+    """
+    Finish generating project structure with cookie cutter.
+    Thanks for using Std Tech Dept Template.
+    """
+))
